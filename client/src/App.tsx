f9a8e76b945bf8c86b9711f5b23ba632deb5072e
Ibra4i <82802664+Ibra4i@users.noreply.github.com>
2023-10-25 14:45:14 +0200

    login,signup
    -...-..

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,9 +27,62 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
+      <Login></Login>
     </>
   )
 }
 
 export default App
+
+function Login() {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [mail, setMail] = useState("")
+  const [gender, setGender] = useState("")
+  const [birthdate, setBirthdate] = useState("1999-01-01")
+  const [isCreatingUser, setIsCreatingUser] = useState(false)
+  
+  if (!isCreatingUser) {
+    return (
+      <>
+        <form onSubmit={e => e.preventDefault()}>
+          <h1>Log ind</h1>
+          <input type='text' value={username} onChange={e => setUsername(e.target.value)} placeholder='Brugernavn'required/>
+          <br/>
+          <input type='password' value={password} onChange={e => setPassword(e.target.value)} placeholder='Adgangskode' required/>
+          <br/>
+          <button type='submit'>Log ind</button>
+          <br/>
+          <a onClick={() => setIsCreatingUser(true)}>Opret ny bruger</a>
+        </form>
+      </>
+    )
+  }
+  else {
+    return (
+      <>
+        <form onSubmit={e => e.preventDefault()}>
+          <h1>Ny bruger</h1>
+          <input type='text' value={username} onChange={e => setUsername(e.target.value)} placeholder='Brugernavn' required/>
+          <br/>
+          <input type='password' value={password} onChange={e => setPassword(e.target.value)} placeholder='Adgangskode' required/>
+          <br/>
+          <input type='email' value={mail} onChange={e => setMail(e.target.value)} placeholder='Email adresse' required/>
+          <br/>
+          <label>Fødselsdato</label>
+          <br/>
+          <input type='date' value={birthdate} onChange={e => setBirthdate(e.target.value)} required/>
+          <br/>
+          <input type='radio' name='gender' id='male' onClick={() => setGender("male")} required></input>
+          <label htmlFor="male">Mand</label>
+          <input type='radio' name='gender' id='female' onClick={() => setGender("female")} required></input>
+          <label htmlFor='female'>Kvinde</label>
+          <button type='submit'>Opret bruger</button>
+          <br/>
+          <a onClick={() => setIsCreatingUser(false)}>Tilbage til login</a>
+        </form>
+      </>
+    )
+  }
+}
