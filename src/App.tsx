79cd58f0aa5d752a1a3da1ea023e215fc7ccbd57
Kristoffer <33570390+Kristoffermg@users.noreply.github.com>
2023-11-14 12:04:37 +0100

    User authentication
    Base setup. Needs:
    -> Access criteria depending on authentication status
    -> Communication with user service through API

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Sidebar from './components/Sidebar/sidebar'
 import { Route, Routes } from 'react-router-dom';
 import { BrowserRouter as Router} from 'react-router-dom';
+import Login from './containers/Login/Login'
 import User from './containers/User/User'
 import MealPlans from './containers/MealPlans/MealPlans'
 import Health from './containers/Health/Health'
@@ -11,9 +12,9 @@ import './App.scss'
 function App() {
   return (
     <>
-      {/* <Login></Login> */}
       <Router>
-        <Sidebar />
+        <Login />
+        {/* <Sidebar />
         <div className='container'>
           <Routes>
             <Route path="user" element={<User />} />
@@ -21,63 +22,10 @@ function App() {
             <Route path="inventory" element={<Inventory />} />
             <Route path="health" element={<Health />} />
           </Routes>
-        </div>
+        </div> */}
       </Router>
     </>
   )
 }
 
 export default App
-
-// function Login() {
-//   const [username, setUsername] = useState("")
-//   const [password, setPassword] = useState("")
-//   const [mail, setMail] = useState("")
-//   const [gender, setGender] = useState("")
-//   const [birthdate, setBirthdate] = useState("1999-01-01")
-//   const [isCreatingUser, setIsCreatingUser] = useState(false)
-  
-//   if (!isCreatingUser) {
-//     return (
-//       <>
-//         <p>{gender}</p>
-//         <form onSubmit={e => e.preventDefault()}>
-//           <h1>Log ind</h1>
-//           <input type='text' value={username} onChange={e => setUsername(e.target.value)} placeholder='Brugernavn'required/>
-//           <br/>
-//           <input type='password' value={password} onChange={e => setPassword(e.target.value)} placeholder='Adgangskode' required/>
-//           <br/>
-//           <button type='submit'>Log ind</button>
-//           <br/>
-//           <a onClick={() => setIsCreatingUser(true)}>Opret ny bruger</a>
-//         </form>
-//       </>
-//     )
-//   }
-//   else {
-//     return (
-//       <>
-//         <form onSubmit={e => e.preventDefault()}>
-//           <h1>Ny bruger</h1>
-//           <input type='text' value={username} onChange={e => setUsername(e.target.value)} placeholder='Brugernavn' required/>
-//           <br/>
-//           <input type='password' value={password} onChange={e => setPassword(e.target.value)} placeholder='Adgangskode' required/>
-//           <br/>
-//           <input type='email' value={mail} onChange={e => setMail(e.target.value)} placeholder='Email adresse' required/>
-//           <br/>
-//           <label>Fødselsdato</label>
-//           <br/>
-//           <input type='date' value={birthdate} onChange={e => setBirthdate(e.target.value)} required/>
-//           <br/>
-//           <input type='radio' name='gender' id='male' onClick={() => setGender("male")} required></input>
-//           <label htmlFor="male">Mand</label>
-//           <input type='radio' name='gender' id='female' onClick={() => setGender("female")} required></input>
-//           <label htmlFor='female'>Kvinde</label>
-//           <button type='submit'>Opret bruger</button>
-//           <br/>
-//           <a onClick={() => setIsCreatingUser(false)}>Tilbage til login</a>
-//         </form>
-//       </>
-//     )
-//   }
-// }
